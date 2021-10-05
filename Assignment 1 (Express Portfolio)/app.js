7fa09369fd45259ff2ec9edb545879373affab38
Mitkumar Malavia <mitm800@gmail.com>
2021-10-05 11:37:28 -0400

    Express Portfolio

--- Assignment 1 (Express Portfolio)/app.js
+++ Assignment 1 (Express Portfolio)/app.js
@@ -1,3 +1,4 @@
+/*Mitkumar Malavia, 301214375, 05/10/2021 */
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -25,18 +26,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- Assignment 1 (Express Portfolio)/routes/index.js
+++ Assignment 1 (Express Portfolio)/routes/index.js
@@ -1,30 +1,31 @@
+/*Mitkumar Malavia, 301214375, 05/10/2021*/
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET method for all the pages. */
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Home' , fileName: 'home' });
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Home', fileName: 'home' });
 });
 
-router.get('/home', function (req, res, next) {
-  res.render('index', { title: 'Home' , fileName: 'home' });
+router.get('/home', function(req, res, next) {
+    res.render('index', { title: 'Home', fileName: 'home' });
 });
 
-router.get('/about', function (req, res, next) {
-  res.render('index', { title: 'About' , fileName: 'about'});
+router.get('/about', function(req, res, next) {
+    res.render('index', { title: 'About', fileName: 'about' });
 });
 
-router.get('/project', function (req, res, next) {
-  res.render('index', { title: 'Projects' , fileName: 'project'});
+router.get('/project', function(req, res, next) {
+    res.render('index', { title: 'Projects', fileName: 'project' });
 });
 
-router.get('/services', function (req, res, next) {
-  res.render('index', { title: 'Services' , fileName: 'services'});
+router.get('/services', function(req, res, next) {
+    res.render('index', { title: 'Services', fileName: 'services' });
 });
 
-router.get('/contact', function (req, res, next) {
-  res.render('index', { title: 'Contact' , fileName: 'contact'});
+router.get('/contact', function(req, res, next) {
+    res.render('index', { title: 'Contact', fileName: 'contact' });
 });
 
 module.exports = router;
